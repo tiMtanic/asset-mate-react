0f7a47ee6ad94438a7172e1fc16476389259dabf
Tim Lehming <timlehming@aol.com>
2026-08-23 18:33:30 +0200

    Configure React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import StocksListPage from "./pages/StocksListPage";
+import StockPage from "./pages/StockPage";
+import AddEditStockPage from "./pages/AddEditStockPage";
+import WatchlistPage from "./pages/WatchlistPage";
+import AboutPage from "./pages/AboutPage";
+
 function App() {
   return (
     <>
       <p>Hello World!</p>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/stocks" element={<StocksListPage />} />
+        <Route path="/stocks/:stockId" element={<StockPage />} />
+        <Route path="/stocks/:stockId/edit" element={<AddEditStockPage />} />
+        <Route path="/watchlist" element={<WatchlistPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
